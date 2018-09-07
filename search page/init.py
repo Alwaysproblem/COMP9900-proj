d1ad983d2c651bf134b5c5b5395c07fb80f01c55
Lacknight <lacknight@Lacknight.local>
2018-09-07 11:23:32 +1000

    project.py

---
+++ search page/init.py
@@ -0,0 +1,101 @@
+import os, re, sqlite3, uuid
+
+conn = sqlite3.connect('small.db',detect_types=sqlite3.PARSE_DECLTYPES,check_same_thread=False)
+
+def create_table():
+    conn.execute("drop table if exists users")
+    conn.execute('''create table users (
+                    ID int primary key not null,
+                    userid char(50) not null,
+                    password char(50) not null,
+                    full_name char(50) not null,
+                    email char(50) not null,
+                    home char(50),
+                    home_latitude char(50),
+                    home_longitude char(50),
+                    home_suburb char(50));
+                ''')
+
+    conn.execute("drop table if exists hotel")
+    conn.execute('''create table hotel (
+                    ID int primary key not null,
+                    hotelid char(50) not null,
+                    userid char(50) ,
+                    hotel char(50),
+                    hotel_latitude char(50),
+                    hotel_longitude char(50),
+                    hotel_suburb char(50),
+                    check_in_date char(50),
+                    check_out_date char(50),
+                    hotel_class char(50),
+                    guest_renting char(50),
+                    room_type char(50),
+                    price int(50));
+                    ''')
+
+    conn.execute("drop table if exists messages")
+    conn.execute('''create table messages (
+                    ID int primary key not null,
+                    mid char(50) not null,
+                    userid char(50) not null,
+                    message char(50) not null,
+                    time char(50));
+                ''')
+
+    conn.execute("drop table if exists comments")
+    conn.execute('''create table comments (
+                   ID int primary key not null,
+                   cid char(50) not null,
+                   mid char(50) not null,
+                   message char(50),
+                   userid char(50) not null,
+                   muserid char(50) not null,
+                   time char(50));
+                ''')
+
+
+def load_hotel_info():
+    conn.execute("delete from hotel")
+    key = '"ID", "hotelid", "hotel", "hotel_class", "guest_renting", "room_type", "price"'
+    value = '"1", "1", "isis", "5", "4", "single", 500'
+    sql = 'insert into hotel (' + key + ') values (' + value + ')'
+    conn.execute(sql)
+    value = '"2", "2", "saf", "4", "3", "double", 400'
+    sql = 'insert into hotel (' + key + ') values (' + value + ')'
+    conn.execute(sql)
+    value = '"3", "3", "afea", "3", "5", "family", 600'
+    sql = 'insert into hotel (' + key + ') values (' + value + ')'
+    conn.execute(sql)
+    value = '"4", "4", "afafe", "2", "4", "multiple", 600'
+    sql = 'insert into hotel (' + key + ') values (' + value + ')'
+    conn.execute(sql)
+    value = '"5", "5", "aeaeae", "1", "1", "double", 200'
+    sql = 'insert into hotel (' + key + ') values (' + value + ')'
+    conn.execute(sql)
+    value = '"6", "6", "isisas", "5", "3", "multiple", 350'
+    sql = 'insert into hotel (' + key + ') values (' + value + ')'
+    conn.execute(sql)
+    conn.commit()
+    # key += ', "' + match.group(1) + '"'
+    # value += ', "' + match.group(2) + '"'
+    # sql = 'insert into students (' + key + ') values (' + value + ')'
+# print(sql)
+#     conn.execute(sql)
+#     conn.commit()
+
+create_table()
+load_hotel_info()
+detial = str(5)
+# sql = 'select * from hotel where hotel_class = 5'
+hotelclass = '-1'
+guestrenting = '-1'
+roomtype = ''
+# sql = 'select ID, hotelid, hotel, hotel_class, guest_renting from hotel '
+sql = 'select * from hotel where '+'(hotel_class = 2 or hotel_class = 1 or hotel_class = 0) and (guest_renting = 2 or guest_renting = 1 or guest_renting = 0) and room_type = "double" order by price desc'
+print(sql)
+# sql = 'select ID, hotelid, hotel, hotel_class from hotel '
+cursor = conn.execute(sql)
+for row in cursor:
+    print(row)
+    # cid = row[0]
+    # mid = row[1]
