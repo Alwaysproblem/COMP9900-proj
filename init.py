5afde8cb025adf3a78c6623792d8efe197d814cd
Alwaysproblem <reganyang0415@gmail.com>
2018-09-07 11:40:31 +1000

    modified

--- init.py
+++ init.py
@@ -52,3 +52,50 @@ def create_table():
                    muserid char(50) not null,
                    time char(50));
                 ''')
+
+
+def load_hotel_info():
+    conn.execute("delete from hotel")
+    key = '"ID", "hotelid", "hotel", "hotel_class", "guest_renting", "room_type", "price"'
+    value = '"1", "1", "isis", "5", "4", "single", 500'
+    sql = 'insert into hotel (' + key + ') values (' + value + ')'
+    conn.execute(sql)
+    value = '"2", "2", "saf", "4", "3", "double", 400'
+    sql = 'insert into hotel (' + key + ') values (' + value + ')'
+    conn.execute(sql)
+    value = '"3", "3", "afea", "3", "5", "family", 600'
+    sql = 'insert into hotel (' + key + ') values (' + value + ')'
+    conn.execute(sql)
+    value = '"4", "4", "afafe", "2", "4", "multiple", 600'
+    sql = 'insert into hotel (' + key + ') values (' + value + ')'
+    conn.execute(sql)
+    value = '"5", "5", "aeaeae", "1", "1", "double", 200'
+    sql = 'insert into hotel (' + key + ') values (' + value + ')'
+    conn.execute(sql)
+    value = '"6", "6", "isisas", "5", "3", "multiple", 350'
+    sql = 'insert into hotel (' + key + ') values (' + value + ')'
+    conn.execute(sql)
+    conn.commit()
+    # key += ', "' + match.group(1) + '"'
+    # value += ', "' + match.group(2) + '"'
+    # sql = 'insert into students (' + key + ') values (' + value + ')'
+# print(sql)
+#     conn.execute(sql)
+#     conn.commit()
+
+create_table()
+load_hotel_info()
+detial = str(5)
+# sql = 'select * from hotel where hotel_class = 5'
+hotelclass = '-1'
+guestrenting = '-1'
+roomtype = ''
+# sql = 'select ID, hotelid, hotel, hotel_class, guest_renting from hotel '
+sql = 'select * from hotel where '+'(hotel_class = 2 or hotel_class = 1 or hotel_class = 0) and (guest_renting = 2 or guest_renting = 1 or guest_renting = 0) and room_type = "double" order by price desc'
+print(sql)
+# sql = 'select ID, hotelid, hotel, hotel_class from hotel '
+cursor = conn.execute(sql)
+for row in cursor:
+    print(row)
+    # cid = row[0]
+    # mid = row[1]
