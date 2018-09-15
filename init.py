f4b150d999a27f7cf8b29a794c1f15c0f6db8adf
Alwaysproblem <reganyang0415@gmail.com>
2018-09-15 16:57:40 +1000

    merge

--- init.py
+++ init.py
@@ -3,52 +3,60 @@ import os, re, sqlite3, uuid
 conn = sqlite3.connect('small.db',detect_types=sqlite3.PARSE_DECLTYPES,check_same_thread=False)
 
 def create_table():
-    conn.execute("drop table if exists users")
-    conn.execute('''create table users (
-                    ID int primary key not null,
-                    userid char(50) not null,
-                    password char(50) not null,
-                    full_name char(50) not null,
-                    email char(50) not null,
-                    home char(50),
-                    home_latitude char(50),
-                    home_longitude char(50),
-                    home_suburb char(50));
-                ''')
-
-    conn.execute("drop table if exists hotel")
-    conn.execute('''create table hotel (
-                    ID int primary key not null,
-                    hotelid char(50) not null,
-                    userid char(50) ,
-                    hotel char(50),
-                    hotel_latitude char(50),
-                    hotel_longitude char(50),
-                    hotel_suburb char(50),
-                    check_in_date char(50),
-                    check_out_date char(50),
-                    hotel_class char(50),
-                    guest_renting char(50),
-                    room_type char(50),
-                    price int(50));
+    with conn:
+        conn.execute("drop table if exists users")
+        conn.execute('''create table users (
+                        ID int primary key not null,
+                        userid char(50) not null,
+                        password char(50) not null,
+                        full_name char(50) not null,
+                        email char(50) not null,
+                        home char(50),
+                        home_latitude char(50),
+                        home_longitude char(50),
+                        home_suburb char(50));
                     ''')
 
-    conn.execute("drop table if exists messages")
-    conn.execute('''create table messages (
-                    ID int primary key not null,
-                    mid char(50) not null,
-                    userid char(50) not null,
-                    message char(50) not null,
-                    time char(50));
-                ''')
+        conn.execute("drop table if exists hotel")
+        conn.execute('''create table hotel (
+                        useremail char(50),
+                        HouseID char(50) primary key not null,
+                        RoomNo char(50),
+                        Street char(50),
+                        Suburb char(50),
+                        State char(50),
+                        Postcode char(50),
+                        RoomType char(50),
+                        Star char(50),
+                        check_in_date char(50),
+                        check_out_date char(50),
+                        price int(50));
+                        ''')
 
-    conn.execute("drop table if exists comments")
-    conn.execute('''create table comments (
-                   ID int primary key not null,
-                   cid char(50) not null,
-                   mid char(50) not null,
-                   message char(50),
-                   userid char(50) not null,
-                   muserid char(50) not null,
-                   time char(50));
-                ''')
+        conn.execute("drop table if exists messages")
+        conn.execute('''create table messages (
+                        ID int primary key not null,
+                        mid char(50) not null,
+                        userid char(50) not null,
+                        message char(50) not null,
+                        time char(50));
+                    ''')
+
+        conn.execute("drop table if exists comments")
+        conn.execute('''create table comments (
+                    ID int primary key not null,
+                    cid char(50) not null,
+                    mid char(50) not null,
+                    message char(50),
+                    userid char(50) not null,
+                    muserid char(50) not null,
+                    time char(50));
+                    ''')
+
+    conn.close()
+
+def main():
+    create_table()
+
+if __name__ == '__main__':
+    main()
